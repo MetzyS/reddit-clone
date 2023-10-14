7c94f66ab3b857abd3b72586248e5bb729bc920f
metzys <contact@metzys.net>
2023-10-14 13:17:56 +0200

    mobile: useState submenu open/close + animation

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { NavButton } from "../UI/navbutton";
+import { useState } from "react";
 import { NavItem } from "../UI/navitem";
 import Image from "next/image";
 import { FaExternalLinkSquareAlt, FaReact } from "react-icons/fa";
@@ -10,12 +11,34 @@ import TailwindIcon from "../../../public/tailwind.svg";
 import GitHubIcon from "../../../public/github.svg";
 
 export default function Navigation(props: { menuIsOpen: boolean }) {
+  // Gestion de l'ouverture des catégories (recent, topics..) dans la nav mobile
+  const initialSubMenuIsOpen = {
+    recent: true,
+    topics: false,
+    resources: false,
+  };
+
+  interface subMenuIsOpen {
+    recent: boolean;
+    topics: boolean;
+    resources: boolean;
+  }
+
+  const [subMenuIsOpen, setSubMenuIsOpen] = useState(initialSubMenuIsOpen);
+  const handleSubMenuClick = (submenu: string) => {
+    const value = subMenuIsOpen[submenu as keyof subMenuIsOpen];
+    setSubMenuIsOpen((prevSubMenuIsOpen) => ({
+      ...prevSubMenuIsOpen,
+      [submenu]: !value,
+    }));
+  };
+
   const Icon = <Image src={ReactIcon} alt="" />;
   return (
     <nav
       className={`fixed flex mt-12 border-0 border-t border-neutral-800 ${
         props.menuIsOpen ? "w-72" : "w-0"
-      } bg-neutral-900 h-full overflow-y-scroll overflow-x-hidden`}
+      } bg-neutral-900 h-full pr-1 overflow-hidden hover:overflow-y-scroll`}
     >
       <ul className="w-full">
         {/* Popular */}
@@ -29,16 +52,24 @@ export default function Navigation(props: { menuIsOpen: boolean }) {
           <button
             type="button"
             className="flex justify-between h-8 items-center hover:bg-neutral-800"
+            onClick={() => handleSubMenuClick("recent")}
           >
             <span className="text-xs tracking-widest px-4 text-neutral-400">
               RECENT
             </span>
             <span className="mr-6">
-              <MdExpandLess className="w-5 h-5 text-neutral-300" />
+              <MdExpandLess
+                className={
+                  subMenuIsOpen.recent
+                    ? "w-5 h-5 text-neutral-300 rotate-180"
+                    : "w-5 h-5 text-neutral-300"
+                }
+              />
             </span>
           </button>
 
-          <div>
+          {/* Recent items */}
+          <div className={subMenuIsOpen.recent ? "block" : "hidden"}>
             <NavItem
               image={NextIcon}
               btnClass="hover:bg-neutral-800"
@@ -65,33 +96,92 @@ export default function Navigation(props: { menuIsOpen: boolean }) {
             />
           </div>
         </div>
+
         {/* Topics */}
         <div className="flex flex-col border-0 border-b border-neutral-800 py-4 mb-2">
           <button
             type="button"
             className="flex justify-between h-8 items-center hover:bg-neutral-800"
+            onClick={() => handleSubMenuClick("topics")}
           >
             <span className="text-xs tracking-widest px-4 text-neutral-400">
               TOPICS
             </span>
             <span className="mr-6">
-              <MdExpandLess className="w-5 h-5 text-neutral-300" />
+              <MdExpandLess
+                className={
+                  subMenuIsOpen.topics
+                    ? "w-5 h-5 text-neutral-300 rotate-180"
+                    : "w-5 h-5 text-neutral-300"
+                }
+              />
             </span>
           </button>
+
+          {/* Topics items */}
+          <div className={subMenuIsOpen.topics ? "block" : "hidden"}>
+            <NavItem
+              image={NextIcon}
+              btnClass="hover:bg-neutral-800"
+              imageWidth={25}
+              text="NextJS"
+            />
+            <NavItem
+              image={NextIcon}
+              btnClass="hover:bg-neutral-800"
+              imageWidth={25}
+              text="NextJS"
+            />
+            <NavItem
+              image={NextIcon}
+              btnClass="hover:bg-neutral-800"
+              imageWidth={25}
+              text="NextJS"
+            />
+          </div>
         </div>
-        {/* Ressources */}
+
+        {/* Ressources category */}
         <div className="flex flex-col  py-3 mt-1 mb-2">
           <button
             type="button"
             className="flex justify-between h-8 items-center hover:bg-neutral-800"
+            onClick={() => handleSubMenuClick("resources")}
           >
             <span className="text-xs tracking-widest px-4 text-neutral-400">
               RESOURCES
             </span>
             <span className="mr-6">
-              <MdExpandLess className="w-5 h-5 text-neutral-300" />
+              <MdExpandLess
+                className={
+                  subMenuIsOpen.resources
+                    ? "w-5 h-5 text-neutral-300 rotate-180"
+                    : "w-5 h-5 text-neutral-300"
+                }
+              />
             </span>
           </button>
+          {/* Resources items */}
+          <div className={subMenuIsOpen.resources ? "block" : "hidden"}>
+            <NavItem
+              image={NextIcon}
+              btnClass="hover:bg-neutral-800"
+              imageWidth={25}
+              text="NextJS"
+            />
+            <NavItem
+              image={NextIcon}
+              btnClass="hover:bg-neutral-800"
+              imageWidth={25}
+              text="NextJS"
+            />
+            <NavItem
+              image={NextIcon}
+              btnClass="hover:bg-neutral-800"
+              imageWidth={25}
+              text="NextJS"
+            />
+          </div>
         </div>
       </ul>
     </nav>
